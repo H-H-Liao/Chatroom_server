501e16745c7890a0f2ecd58e4051696034d81a77
H-H-Liao <liao@wanliroad.com>
2022-11-07 23:18:26 +0800

    refactor:重構聊天室流程

--- server.js
+++ server.js
@@ -6,34 +6,65 @@ import dotenv from 'dotenv'
 const port = parseInt(dotenv.config('PORT',80));
 const wss = new WebSocketServer({ port: 8088 });
 
-let $list = [];
-let $id = 0;
+//客服人員列表
+let $staff_list = [];
+
+//顧客端列表
+let $client_list = [{
+                        username:"王小明",
+                        user_id:1
+                    },
+                    {
+                        username:"陳聰明",
+                        user_id:2
+                    }];
+
+console.log('啟動聊天室伺服器');
 
 wss.on('connection', function connection(ws, req) {
+    //抓取傳入的token
     const $url = url.parse(req.url, true);
-    let $username = $url.query.username;
+    const $token = $url.query.token;
 
-    //檢查是否有重複的使用者
-    if(contains($list, $username)) {
-        $username = null;
+    //呼叫api server檢驗身份
+    const $check_information = checkToken($token)
+
+    ws.uid = $check_information.user_id;//存入user_id
+    
+    if(!$check_information.success){//驗證失敗踢出聊天室
         ws.close();
-    }else{
-        console.log($username + 'Open connection')
-        $list.push($username);
-        ws.id = $id++;
-        //通知所有人在線使用者列表
-        noticeAllUserList(wss,$list);
     }
 
+    if(hasRepeatLogin()){//移除重複登入的使用者
+        removeRepeatLoginUser();
+    }
+
+
+    //根據角色載入資料
+    switch($check_information.role){
+        case 'staff':
+             //客服人員載入所有服務的顧客
+            break;
+        case 'client':
+            //顧客端載入聊天室內最近10筆記錄
+            break;
+    }
+    
     ws.on('message', function message(data) {
-        const $obj = {
-            type: "message",
-            user:$username,
-            content:String(data)
-        };
-        wss.clients.forEach(client => {
-            client.send(JSON.stringify($obj));
-        })
+        const $json = JSON.parse(String(data));
+        //根據傳入的類型配適道相對應的function
+        switch($json.type){
+            case "go_client_room"://客服人員獲取顧客的room
+                ws.send(getClientRoom());
+                break;
+            case "message"://傳送訊息
+                senMessage(ws.uid, $json.to_uid,$json.message);
+                break;
+            case "history"://要求歷史對話
+                break;
+            case "close"://手動點選結束對話
+                break;
+        }
     });
 
     ws.on('close', () => {
@@ -55,5 +86,39 @@ const noticeAllUserList = (wss, $list) => {
             list:$list
         };
         client.send(JSON.stringify($obj));
+    });
+}
+
+const checkToken = ($token) => {
+    return {
+        success:true,
+        role:'staff',
+        user_id:1
+    };
+}
+
+const hasRepeatLogin = () => {
+    return true;
+}
+
+const removeRepeatLoginUser = () => {
+
+}
+
+const getClientRoom = () => {
+    const $object = {
+        room_list: $client_list
+    }
+    return JSON.stringify($object);
+}
+
+const senMessage = ($from, $to, $role, $message) => {
+    //存入資料庫
+    wss.clients.forEach(function (client) {
+        if(client.uid == $from){
+            client.send($message);
+        }else if(client.uid == $to){
+            client.send($message);
+        }
     });
 }
